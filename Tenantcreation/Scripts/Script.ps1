c634f75f9eefb00dbe7c8f70633462cf974895e2
sreenathakk <sreenatha.kk@gmail.com>
2018-05-21 18:51:02 +0530

    Update Script.ps1

--- Tenantcreation/Scripts/Script.ps1
+++ Tenantcreation/Scripts/Script.ps1
@@ -44,15 +44,15 @@ Param(
  
 )
 
-function Disable-IEESC{
+try {
 $AdminKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
 $UserKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
 Set-ItemProperty -Path $AdminKey -Name “IsInstalled” -Value 0
 Set-ItemProperty -Path $UserKey -Name “IsInstalled” -Value 0
 Stop-Process -Name Explorer
-Write-Host “Done Disabling IE Enhanced Security Configuration.” -ForegroundColor Green
+“IE Enhanced Security Configuration (ESC) has been disabled on this machine.” 
 }
-Disable-IEESC
+catch {"Failed to disable IE ESC" }
 Invoke-WebRequest -Uri $fileURI -OutFile "C:\PSModules.zip"
 Start-Sleep -Seconds 30
 New-Item -Path "C:\PSModules" -ItemType directory -Force -ErrorAction SilentlyContinue
